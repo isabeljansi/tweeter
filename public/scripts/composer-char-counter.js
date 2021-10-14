d2c9f50c6a662632364cafe802a4018cb6f278bd
Isabel Jansi Peter Perianayagam <isabeljansi1988@gmail.com>
2021-10-14 16:14:56 -0400

    reset the character count

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,18 +3,7 @@
 $(document).ready(function() {
 console.log("Test: composer-char-counter.js file has loaded to the DOM")
 
-
-// $( ".textArea" ).change(function() {
-//   alert( "Handler for .change() called when a change occurs in text area." );
-// });
-
-// This will add a click event listener to all elements with the "button" class
-// $(".button").on("click", (event) => {
-//   alert( "You clicked the Tweet button." );
-
-// })
-
-$( "textArea" ).keyup(function() {
+$( ".textArea" ).keyup(function() {
   let myTweetLength = $(this).val().length;
   console.log(myTweetLength);
   $(".counter").text (140 - myTweetLength);
